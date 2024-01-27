fed06ceb89ddc261130df1c587164890358cdc0b
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-27 01:06:30 -0300

    Author CRUD

--- models/author.py
+++ models/author.py
@@ -3,5 +3,6 @@ from database import db
 class Author(db.Model):
   id = db.Column(db.Integer, unique=True, primary_key=True)
   name = db.Column(db.String(64), nullable=False)
+  photo = db.Column(db.String(255), nullable=False)
 
   books = db.relationship('Book', backref='author', lazy=True)
